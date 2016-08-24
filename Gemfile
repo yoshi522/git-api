f54f197e8e679315110c5c55f435a40f08847ee4
yoshi522 <yoshihito522@gmail.com>
2016-08-24 23:27:56 +0900

    update

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'rest-client'
+gem 'haml'
+
+group :development do
+  gem 'foreman'
+  gem 'heroku'
+end
--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
+require 'bundler/setup'
 require 'sinatra'
 
 get '/' do
-  'Hello yoshi!'
+  code = "<%= Time.now %>"
+  erb code
 end
 
 get '/v1' do
-  'gif api version is 1.0'
+  @version = 'gif api version is 1.0'
+  erb :v1
+end
+
+get '/v1/gif' do
+  num = rand(8) + 1
+  @gif_url = 'https://s3-ap-northeast-1.amazonaws.com/line-bot-2016/gif/git-' + num.to_s + '.gif'
+  erb :gif
+end
+
+get '/v1/gif/preview' do
+  num = rand(8) + 1
+  @gif_url = 'https://s3-ap-northeast-1.amazonaws.com/line-bot-2016/gif/git-' + num.to_s + '.gif'
+  erb :preview
 end
